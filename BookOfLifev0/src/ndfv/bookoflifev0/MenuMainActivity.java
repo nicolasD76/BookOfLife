36fabdbc7ebc6b229ab385d876a97228ad79af05
isen <isen@ISENMIC-CRRQHR0>
2015-05-01 22:22:26 +0200

    Commit des classes activité pour les deux écran: Selection d'un compteur et visionnage des stats

--- BookOfLifev0/src/ndfv/bookoflifev0/MenuMainActivity.java
+++ BookOfLifev0/src/ndfv/bookoflifev0/MenuMainActivity.java
@@ -1,8 +1,9 @@
 package ndfv.bookoflifev0;
 
-import ndfv.bookoflifev0.R;
+import com.example.bookoflifev0.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -10,16 +11,20 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MenuMainActivity extends Activity implements OnClickListener{
-	private Button compteurButton = null;
-	private int numberOfCompteur = 0;
-	private String compteurLabel = "Compteur";
+	private Button compteursButton = null;
+	private Button statsButton = null;
+//	private int numberOfCompteur = 0;
+//	private String compteurLabel = "Compteur";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		compteurButton = (Button) findViewById(R.id.counters_button);
-        compteurButton.setOnClickListener(this);
+		setContentView(R.layout.activity_menu_main);
+		compteursButton = (Button) findViewById(R.id.counters_button);
+        compteursButton.setOnClickListener(this);
+		statsButton = (Button) findViewById(R.id.stats_button);
+		statsButton.setOnClickListener(this);
+
 
 	}
 
@@ -36,12 +41,21 @@ public class MenuMainActivity extends Activity implements OnClickListener{
 		case R.id.counters_button:
 			onButtonCompteursClick();
 			break;
+		case R.id.stats_button:
+			onButtonStatsClick();
+			break;
 		}
 	}
 	
 	public void onButtonCompteursClick(){
-		numberOfCompteur = numberOfCompteur + 1;
-		compteurButton.setText(compteurLabel + "\n" + numberOfCompteur);
+//		numberOfCompteur = numberOfCompteur + 1;
+		Intent activitySelectionCounters = new Intent(this,SelectionCountersActivity.class);
+		startActivity(activitySelectionCounters);
+	}
+	
+	public void onButtonStatsClick(){
+		Intent activityStats = new Intent(this,StatsActivity.class);
+		startActivity(activityStats);
 	}
 
 }
