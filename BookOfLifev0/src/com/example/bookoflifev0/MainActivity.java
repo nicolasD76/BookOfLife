4e349dc2358426314a4317bd5d8dc378e1be39db
isen <isen@ISENMIC-CRRQHR0>
2015-05-01 21:55:21 +0200

    Commit de ce qu'on avait déjà dev

--- BookOfLifev0/src/com/example/bookoflifev0/MainActivity.java
+++ BookOfLifev0/src/com/example/bookoflifev0/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.bookoflifev0;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
-	private int nicoD;
+public class MainActivity extends Activity implements OnClickListener{
+	private Button compteurButton = null;
+	private int numberOfCompteur = 0;
+	private String compteurLabel = "Compteur";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		compteurButton = (Button) findViewById(R.id.counters_button);
+        compteurButton.setOnClickListener(this);
+
 	}
 
 	@Override
@@ -18,6 +27,20 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()){
+		case R.id.counters_button:
+			onButtonCompteursClick();
+			break;
+		}
+	}
+	
+	public void onButtonCompteursClick(){
+		numberOfCompteur = numberOfCompteur + 1;
+		compteurButton.setText(compteurLabel + "\n" + numberOfCompteur);
 	}
 
 }
