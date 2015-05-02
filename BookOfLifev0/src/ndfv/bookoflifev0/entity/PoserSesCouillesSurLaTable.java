f2f4c48538a06429660bf997b960d50131af1dbd
ISEN <ISEN@Vdcibel>
2015-05-02 13:41:29 +0200

    Remove Poser Ses Couilles Sur La Table By Elrich

--- BookOfLifev0/src/ndfv/bookoflifev0/entity/PoserSesCouillesSurLaTable.java
+++
@@ -1,8 +0,0 @@
-package ndfv.bookoflifev0.entity;
-
-public class PoserSesCouillesSurLaTable {
-	private int couilles = 2;
-	private int taille = 50;
-	private String name = "bigbob";
-	private String owner = "NicoD"; 
-}
