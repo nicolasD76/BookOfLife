de0fdd4c29c536e9217b1aa052f6523dc75c303b
isen <isen@ISENMIC-CRRQHR0>
2015-05-02 13:33:42 +0200

    test resolution conflict

--- BookOfLifev0/src/ndfv/bookoflifev0/entity/PoserSesCouillesSurLaTable.java
+++ BookOfLifev0/src/ndfv/bookoflifev0/entity/PoserSesCouillesSurLaTable.java
@@ -3,4 +3,6 @@ package ndfv.bookoflifev0.entity;
 public class PoserSesCouillesSurLaTable {
 	private int couilles = 2;
 	private int taille = 50;
+	private String name = "bigbob";
+	private String owner = "NicoD";
 }
