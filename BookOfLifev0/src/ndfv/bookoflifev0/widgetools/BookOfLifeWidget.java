c411038751ce2e10156c7506a6687d1cb0571747
nicolasD76 <nicolas.dauge@isen-lille.fr>
2015-05-03 20:30:25 +0200

    bookOfLifeWidget connected to app

--- BookOfLifev0/src/ndfv/bookoflifev0/widgetools/BookOfLifeWidget.java
+++ BookOfLifev0/src/ndfv/bookoflifev0/widgetools/BookOfLifeWidget.java
@@ -1,13 +1,18 @@
 package ndfv.bookoflifev0.widgetools;
 
+import ndfv.bookoflifev0.bookoflifev0.R;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
-
-import ndfv.bookoflifev0.bookoflifev0.R;
+import android.widget.Toast;
 
 public class BookOfLifeWidget extends AppWidgetProvider {
+	
+	public static final String ACTION_SHOW_NOTIFICATION = "ndfv.bookoflifev0.widgetoolsSHOW_NOTIFICATION";
+
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 		final int N = appWidgetIds.length;
@@ -20,18 +25,43 @@ public class BookOfLifeWidget extends AppWidgetProvider {
 		}
 	}
 
-	/**
-	 * Update the widget
-	 * 
-	 * @param context
-	 * @param appWidgetManager
-	 * @param appWidgetId
-	 */
 	public static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
 
-		// Prepare widget views
-		RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.bookoflife_widget_layout);
-		// views.setTextViewText(R.id.nap_time, "Erase me");
-		appWidgetManager.updateAppWidget(appWidgetId, views);
+	    // Prepare widget views
+	    RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.bookoflife_widget_layout);
+
+	    // Prepare intent to launch on widget click
+	    Intent intent = new Intent(context, BookOfLifeWidget.class);
+	    intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+	    intent.setAction(ACTION_SHOW_NOTIFICATION);
+
+	    // Launch intent on widget click
+	    PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+	    views.setOnClickPendingIntent(R.id.bookoflife_widget, pendingIntent);
+
+	    appWidgetManager.updateAppWidget(appWidgetId, views);
+	}
+	
+	/**
+	 * Handle new messages
+	 */
+	@Override
+	public void onReceive(Context context, Intent intent) {
+	    super.onReceive(context, intent);
+
+	    if (ACTION_SHOW_NOTIFICATION.equals(intent.getAction())) {
+	        showNotification(context);
+	    }
+	}
+
+	/**
+	 * Displays a notification message
+	 * @param context
+	 */
+	protected void showNotification(Context context) {
+	    CharSequence message = "incremente";
+	    int duration = Toast.LENGTH_SHORT;
+	    Toast toast = Toast.makeText(context, message, duration);
+	    toast.show();
 	}
 }
